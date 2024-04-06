e1c9e8f086d26f1f71bf244041b4cf2e7687232c
Norenz Laurito <laurito.marknorenz@gmail.com>
2024-04-06 17:58:30 +0800

    Add ping function

--- src/Player.gd
+++ src/Player.gd
@@ -1,13 +1,17 @@
 extends KinematicBody2D
 
+const ping = preload('res://src/ping.tscn')
 export var speed: int = 200
 export var jumpForce: int = 600
 export var gravity: int = 800
 
 var velocity: Vector2 = Vector2()
 var grounded: bool = false
+var isPinging: bool = false
+var pingNode: KinematicBody2D = null
 
 onready var sprite = $Sprite
+onready var timer = $Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -32,7 +36,26 @@ func _physics_process(delta):
 		sprite.flip_h = true
 	elif velocity.x > 0:
 		sprite.flip_h = false
+		
+	if Input.is_action_pressed("ping") and !isPinging:
+		isPinging = true
+		ping()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func ping():
+	timer.start(3)
+	pingNode = ping.instance()
+	
+	get_parent().add_child(pingNode)
+	
+	pingNode.position = $Position2D.global_position
+	timer.connect("timeout", self, "_on_Timer_timeout")
+	
+func _on_Timer_timeout():
+	isPinging = false
+	if pingNode != null:
+		pingNode.queue_free()
+

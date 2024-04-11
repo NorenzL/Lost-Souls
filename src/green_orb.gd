c4404597fc53b5ada31b2c583c698380816be9a5
Zymon Clark Mendoza <clarkmendoza21@gmail.com>
2024-04-11 08:35:53 +0800

    add temp orb collision scripts

---
+++ src/green_orb.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var overlap = get_overlapping_areas()
+	for node in overlap:
+		print ("Green Collided!")
+		$Sprite.position.y -= 15
+		$Sprite.position.x += 15

4407aa8def9466398b82a3359991b060401c81b1
Zymon Clark Mendoza <clarkmendoza21@gmail.com>
2024-05-01 08:12:42 +0800

    Add network setup script

---
+++ src/scripts/Network_setup.gd
@@ -0,0 +1,31 @@
+extends Control
+
+#vars used in ui nodes
+onready var multiplayer_config_ui = $multiplater_configure
+onready var server_ip_address = $multiplayer_configure/Server_ip_address
+
+onready var device_ip_address = $CanvasLayer/Device_ip_address
+
+func _ready():
+	get_tree().connect("network_peer_connected",self,"_player_connected")
+	get_tree().connect("network_peer_disconnected",self, "_player_disconnected")
+	get_tree().connect("connected_to_server",self,"_connected_to_server")
+	
+	#just UI set label text
+	device_ip_address.text = Network.ip_address
+
+func _player_connected(id) -> void:
+	print("player ", str(id), " has connected")
+
+func _player_disconnected(id) -> void:
+	print("player ", str(id), " has disconnected")
+
+func _on_create_server_pressed():
+	multiplayer_config_ui.hide()
+	Network.create_server()
+	
+func on_join_server_pressed():
+	if server_ip_address.text != "":
+		multiplayer_config_ui.hide()
+		Network.ip_address = server_ip_address.text
+		Network.join_server()

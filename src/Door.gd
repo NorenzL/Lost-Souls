3c17d4a475a41bde81403e58c5296ceeea9f7865
Enzo <114037092+LA4th11@users.noreply.github.com>
2024-05-25 21:13:17 +0800

    !-- Orb-Exit --!
    Adding Comments

--- src/Door.gd
+++ src/Door.gd
@@ -8,20 +8,23 @@ func _ready():
 #func _process(delta):
 #    pass
 
+# Called when a body enters the door area.
 func _on_Door_body_entered(body):
-	if body.name in Global.player_id:  # Check if the entering body is a player
-		var player_index = Global.player_id.find(body.name)  # Get the index of the player
+	# Check if the entering body is a player
+	if body.name in Global.player_id:
+		# Get the index of the player
+		var player_index = Global.player_id.find(body.name)
 		if player_index != -1:
+			# Get the required orb color for the player
 			var required_orb_color = match_player_id_to_orb_color(player_index)
 			if required_orb_color != null:
+				# Check if the player has enough orbs of the required color
 				if OrbCounter.checkRequiredOrbs(required_orb_color, 2):
 					print("Player " + str(player_index) + " can exit the game.")
 					# Add your exit game logic here
 					$AnimationPlayer.play("Door_Open")
 				else:
 					print("Player " + str(player_index) + " does not have enough orbs to exit.")
-			else:
-				print("Player " + str(player_index) + " does not have a specific orb color assigned.")
 		else:
 			print("Player " + body.name + " not found in Global.player_id.")
 	else:
--- src/OrbCounter.gd
+++ src/OrbCounter.gd
@@ -1,30 +1,37 @@
 extends Node
 
+# Variables to track the number of orbs collected for each color
 var blueOrbsCollected: int = 0
 var greenOrbsCollected: int = 0
 var redOrbsCollected: int = 0
 var yellowOrbsCollected: int = 0
 
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
+# Increment the number of blue orbs collected and print the updated count.
 func incrementBlueOrbs():
 	blueOrbsCollected += 1
 	print("Blue orbs collected: ", blueOrbsCollected)
 
+# Increment the number of green orbs collected and print the updated count.
 func incrementGreenOrbs():
 	greenOrbsCollected += 1
 	print("Green orbs collected: ", greenOrbsCollected)
 
+# Increment the number of red orbs collected and print the updated count.
 func incrementRedOrbs():
 	redOrbsCollected += 1
 	print("Red orbs collected: ", redOrbsCollected)
 
+# Increment the number of yellow orbs collected and print the updated count.
 func incrementYellowOrbs():
 	yellowOrbsCollected += 1
 	print("Yellow orbs collected: ", yellowOrbsCollected)
 
-# Function to check if the player has collected the required number of orbs of a specific color
+# Function to check if the player has collected the required number of orbs of a specific color.
+# Returns true if the required number of orbs of the specified color have been collected, false otherwise.
 func checkRequiredOrbs(color: String, requiredCount: int) -> bool:
 	match color:
 		"blue":

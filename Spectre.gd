6540edacf534977485c1f301333c17cb25563214
Norenz Laurito <laurito.marknorenz@gmail.com>
2024-06-06 18:39:15 +0800

    Add spectre handling of targets

--- Spectre.gd
+++ Spectre.gd
@@ -7,36 +7,54 @@ onready var playerdetection = $playerdetection
 
 
 var _velocity := Vector2.ZERO
+var target_queue := []
 
-var current_target = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	#_timer.connect("timeout", self, "_on_playedetection_body_entered")
 	playerdetection.connect("body_entered", self, "_on_playerdetection_body_entered")
+	playerdetection.connect("body_exited", self, "_on_playerdetection_body_exited")
 	
 func _physics_process(delta: float) -> void:
 	
 	if _agent.is_navigation_finished():
 		return 
-	if current_target:
-		_agent.set_target_location(current_target.global_position)
-		var direction := global_position.direction_to(_agent.get_next_location())
-		var desired_velocity := direction * 200
-		var steering := (desired_velocity - _velocity) * delta * 4.0
-		_velocity += steering
-		_velocity = move_and_slide(_velocity)
+	if target_queue.size() > 0:
+		var current_target = target_queue[0]
+		if current_target:
+			_agent.set_target_location(current_target.global_position)
+			var direction := global_position.direction_to(_agent.get_next_location())
+			var desired_velocity := direction * 200
+			var steering := (desired_velocity - _velocity) * delta * 4.0
+			_velocity += steering
+			_velocity = move_and_slide(_velocity)
+		else:
+			target_queue.pop_front() # Remove the target if it is no longer valid
 
-func _update_pathfinding(location) -> void:
-	_agent.set_target_location(current_target.global_position)
+func _update_pathfinding() -> void:
+	if target_queue.size() > 0:
+		var current_target = target_queue[0]
+		_agent.set_target_location(current_target.global_position)
 
 func _on_playerdetection_body_entered(body):
-	if body is KinematicBody2D:
-		
-		current_target = body
-		_update_pathfinding(body.global_position)
+	if body is KinematicBody2D && !(body in target_queue) && !(body.isDead):
+		print(body.isDead)
+		print(body.name, " is detected")
+		print("location is: ", body.global_position)
+		target_queue.append(body)
+		if target_queue.size() == 1:
+			_update_pathfinding()
 
 
 func _on_playerTouch_body_entered(body):
 	if body is KinematicBody2D:
 		body.die()
+		target_queue.erase(body)
+
+func _on_playerdetection_body_exited(body):
+	if body in target_queue:
+		target_queue.erase(body)
+		print(body.name, " exited detection area")
+		if target_queue.size() > 0:
+			_update_pathfinding()

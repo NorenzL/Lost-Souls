a0f0c60c7c8b4f30ed05e02e0a5b294e29403c82
HydGea <115512432+HydGea@users.noreply.github.com>
2024-06-22 20:59:22 +0800

    Fixed Forced Disconnection Bug
    Removed unnecesary assets

--- Simple_Prompt.gd
+++ Simple_Prompt.gd
@@ -2,6 +2,8 @@ extends Control
 
 
 func _on_Ok_pressed():
+	Global.player_id.resize(0)
+	Global.number_of_players = 0
 	get_tree().change_scene("res://src/Network_setup.tscn")
 
 func set_text(text) -> void:
